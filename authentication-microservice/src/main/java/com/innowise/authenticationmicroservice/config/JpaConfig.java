3ae6a8104fe4a12f6a83217b049563f97d522314
Lelikbjmak <denissolovey990@gmail.com>
2023-07-07 18:56:04 +0300

    feature: Implement JpaConfig for external Entity and Repository classes.

---
+++ authentication-microservice/src/main/java/com/innowise/authenticationmicroservice/config/JpaConfig.java
@@ -0,0 +1,12 @@
+package com.innowise.authenticationmicroservice.config;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EntityScan(basePackages = "com.innowise.usercommon.domain")
+@EnableJpaRepositories(basePackages = "com/innowise/usercommon/repository")
+public class JpaConfig {
+
+}
