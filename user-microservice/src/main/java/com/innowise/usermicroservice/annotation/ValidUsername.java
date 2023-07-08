7bc0b5f384b3aaea27ea3b94c951a09cc2d893f4
Lelikbjmak <denissolovey990@gmail.com>
2023-07-08 17:33:29 +0300

    feature: Implement ValidUsername annotation.

---
+++ user-microservice/src/main/java/com/innowise/usermicroservice/annotation/ValidUsername.java
@@ -0,0 +1,26 @@
+package com.innowise.usermicroservice.annotation;
+
+import com.innowise.usermicroservice.validation.UsernameConstraintValidator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+import java.lang.annotation.*;
+
+import static com.innowise.usermicroservice.util.ApplicationConstant.USERNAME_REGEX_PATTERN;
+
+@Documented
+@Target(value = ElementType.FIELD)
+@Retention(value = RetentionPolicy.RUNTIME)
+@NotBlank(message = "Username is mandatory.")
+@Constraint(validatedBy = UsernameConstraintValidator.class)
+@Pattern(regexp = USERNAME_REGEX_PATTERN, message = "Not valid format.")
+public @interface ValidUsername {
+
+    String message() default "Invalid username.";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
