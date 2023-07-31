737744af7b84439494e3a8814b004dae93602c58
Lelikbjmak <denissolovey990@gmail.com>
2023-07-31 13:32:11 +0300

    feature: Implement JWT-COMMON-TEST module.

---
+++ jwt-common-test/src/main/java/com/innowise/jwtcommontest/security/TestUserDetails.java
@@ -0,0 +1,53 @@
+package com.innowise.jwtcommontest.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+public class TestUserDetails implements UserDetails {
+
+    private final Set<String> roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    public String getPassword() {
+        return "mockPassword";
+    }
+
+    @Override
+    public String getUsername() {
+        return "mockUsername";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
