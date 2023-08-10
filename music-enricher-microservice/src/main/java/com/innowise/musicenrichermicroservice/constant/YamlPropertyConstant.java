f2a04baa298ddeaab3a245bc4374f24121dbfb0c
Lelikbjmak <denissolovey990@gmail.com>
2023-08-10 15:32:34 +0300

    feature: Implement AWS connection properties;

--- music-enricher-microservice/src/main/java/com/innowise/musicenrichermicroservice/constant/YamlPropertyConstant.java
+++ music-enricher-microservice/src/main/java/com/innowise/musicenrichermicroservice/constant/YamlPropertyConstant.java
@@ -12,4 +12,7 @@ public class YamlPropertyConstant {
     public static final String IN_MEMORY_USER_PASSWORD_PROPERTY = "${authentication.basic.credentials.password}";
     public static final String IN_MEMORY_USER_ROLE_PROPERTY = "${authentication.basic.credentials.role}";
 
+    public static final String AWS_ENDPOINT_URL_PROPERTY = "${aws.endpoint-url}";
+    public static final String AWS_ACCESS_KEY_PROPERTY = "${aws.credentials.access-key}";
+    public static final String AWS_SECRET_KEY_PROPERTY = "${aws.credentials.secret-key}";
 }
--- sound-file-microservice/src/main/java/com/innowise/soundfilemicroservice/constant/YamlPropertyConstant.java
+++ sound-file-microservice/src/main/java/com/innowise/soundfilemicroservice/constant/YamlPropertyConstant.java
@@ -6,5 +6,6 @@ import lombok.experimental.UtilityClass;
 public class YamlPropertyConstant {
 
     public static final String AWS_ENDPOINT_URL_PROPERTY = "${aws.endpoint-url}";
-
+    public static final String AWS_ACCESS_KEY_PROPERTY = "${aws.credentials.access-key}";
+    public static final String AWS_SECRET_KEY_PROPERTY = "${aws.credentials.secret-key}";
 }
